240956f31c393f2e30da72459bbeef5d2a69fae9
HoangHuuNghi <hoanghuunghi258@gmail.com>
2024-05-05 01:41:32 +0700

    add 2 files to start mining

---
+++ Setting_Before_Mining.sh
@@ -0,0 +1,5 @@
+sudo su                 # lấy quyền admin
+apt-get update -y       # update ubuntu, ko chạy cũng dc
+apt-get upgrade -y      # update ubuntu, ko chạy cũng dc
+apt-get install git -y  # cài đặt git
+git clone https://github.com/HoangHuuNghi/Coin_Mining.git
