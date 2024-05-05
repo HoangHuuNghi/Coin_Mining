aa237415778ac101bb9c2de941f420f23d47785c
HoangHuuNghi <hoanghuunghi258@gmail.com>
2024-05-05 09:22:21 +0700

    .

--- Start_Mining.sh
+++ Start_Mining.sh
@@ -4,7 +4,7 @@ export Coin_Name=Dero
 case $Coin_Name in
 Dero)
 	git pull
-	pushd $Cur_Dir/Dero
+	pushd Dero
 	chmod u+x astrominer    # cấp quyền cho file chạy
 	chmod u+x rpc_mining.sh # cấp quyền cho file chạy
 	sh rpc_mining.sh
