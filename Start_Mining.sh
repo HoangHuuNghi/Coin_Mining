2bb48f0c5be49900635705c1c4e2b780153e219c
HoangHuuNghi <hoanghuunghi258@gmail.com>
2024-05-05 09:17:26 +0700

    update file start_mining

--- Start_Mining.sh
+++ Start_Mining.sh
@@ -1,12 +1,14 @@
+export Cur_Dir=/home/userland/Coin_mining
+#export Cur_Dir=/d/Git_Repo/Coin_Mining
 export Coin_Name=Dero
 case $Coin_Name in
 Dero)
 	git pull
-	cd Dero/
-	#chmod u+x astrominer    # cấp quyền cho file chạy
-	#chmod u+x rpc_mining.sh # cấp quyền cho file chạy
-	#sh rpc_mining.sh;;
-	echo "test thử chơi";;
+	pushd $Cur_Dir/Dero
+	chmod u+x astrominer    # cấp quyền cho file chạy
+	chmod u+x rpc_mining.sh # cấp quyền cho file chạy
+	sh rpc_mining.sh;;
+	popd;;
 *)
     echo "Please choose the correct coin name" ;;
 esac
